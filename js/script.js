03321dd118cf61905929ef5ef8f3ada33d36b7e5
RobMota <xaparxapando@gmail.com>
2020-10-20 21:30:20 -0300

    Done

--- js/script.js
+++ js/script.js
@@ -1,17 +1,40 @@
 let prod_array = [
   {
     imgPath: "./images/calca.jpg",
-    prod_name: "camiseta",
-    prod_preco: 100,
-    estoque: 5,
+    prod_name: "Calça",
+    prod_preco: 99.99,
+    estoque: 3,
   },
   {
-    imgPath: "./images/calca.jpg",
-    prod_name: "camiseta",
-    prod_preco: 10,
-    estoque: 5,
+    imgPath: "./images/blusa.jpg",
+    prod_name: "Blusa Yu yu Hakusho",
+    prod_preco: 149.99,
+    estoque: 4,
   },
-
+  {
+    imgPath: "./images/sobretudo.jpg",
+    prod_name: "Sobretudo Akatsuke",
+    prod_preco: 2.5,
+    estoque: 6,
+  },
+  {
+    imgPath: "./images/sapato.jpg",
+    prod_name: "Sapato Social",
+    prod_preco: 0.99,
+    estoque: 3,
+  },
+  {
+    imgPath: "./images/naruto.jpg",
+    prod_name: "fantasia naruto",
+    prod_preco: 23456.78,
+    estoque: 2,
+  },
+  {
+    imgPath: "./images/sakura.jpg",
+    prod_name: "fantasia sakura",
+    prod_preco: 4567.89,
+    estoque: 10,
+  },
 ];
 
 let carrinho = document.querySelector(".carrinho");
@@ -38,7 +61,7 @@ prod_array.map((item, index) => {
 
 //adicionar item ao carrinho e apresentar total
 let totalValue = document.querySelector(".totalValue");
-let total = 0.0;
+let total = 0;
 
 function comprarItem(index) {
   let preco = parseFloat(prod_array[index].prod_preco).toFixed(2);
@@ -56,15 +79,20 @@ function comprarItem(index) {
   } else {
     let liElement = `
     <li id="prod-add-${index}">
-    <button class="close" onclick="remover(${index})">X</button>
-    <img src=${prod_array[index].imgPath} alt=${prod_array[index].prod_name} />
-    <span>Quantidade: <strong class="quantValor"> 1 </strong></span>
-    <span>Preço: R$ <strong class="precoValor"> ${preco} </strong></span>
+      <button class="close" onclick="remover(${index})">X</button>
+      <img src=${prod_array[index].imgPath} alt=${prod_array[index].prod_name} />
+      <span>Quantidade: <strong class="quantValor"> 1 </strong></span>
+      <div class="upDown">
+        <div class="setaCima" onclick="up(${index})"></div>
+        <div class="setaBaixo" onclick="down(${index})"></div>
+      </div>
+      <span>Preço: R$ <strong class="precoValor"> ${preco} </strong></span>
     </li>
     `;
 
     carrinho.insertAdjacentHTML("beforeend", liElement);
   }
+  calculaTotal();
 }
 
 // apresentar e esconder carrinho
@@ -84,12 +112,71 @@ showCar.addEventListener("click", () => {
 //remover item do carrinho e atualiza o valor de total
 function remover(index) {
   let item = document.querySelector(`#prod-add-${index}`);
-  let totalItem = item.querySelector(".precoValor");
-
-  total = total - totalItem.innerHTML;
-  totalValue.innerHTML = parseFloat(total).toFixed(2);
 
   item.remove();
+  calculaTotal();
 }
 
+function calculaTotal() {
+  //pega todos os preços do carrinho
+  let precosNoCarrinho = document.getElementsByClassName("precoValor");
+  let totalCarrinho = 0;
 
+  //soma todos os preços do carrinho
+  for (let i = 0; i < precosNoCarrinho.length; i++) {
+    totalCarrinho += parseFloat(precosNoCarrinho[i].innerHTML);
+  }
+
+  //atualiza innerHTML do valor total
+  document.querySelector(".totalValue").innerHTML = totalCarrinho.toFixed(2);
+}
+
+// aumentar a quantidade e valor total do item no carrinho
+// e somar ao valor total de todos os items no carrinho
+function up(index) {
+  let prodAdicionado = document.querySelector(`#prod-add-${index}`);
+  let setaCima = prodAdicionado.querySelector(".setaCima");
+  let estoque = prod_array[index].estoque;
+  let quantValor = prodAdicionado.querySelector(".quantValor"); // quantidade item no carrinho
+  let precoValor = prodAdicionado.querySelector(".precoValor"); //  valor total item no carrinho
+
+  if (quantValor.innerHTML >= estoque) {
+    setaCima.style.borderBottom = "15px solid red";
+    setTimeout(() => {
+      setaCima.style.borderBottom = "15px solid black";
+    }, 200);
+
+    return;
+  }
+
+  quantValor.innerHTML = parseInt(quantValor.innerHTML) + 1;
+  precoValor.innerHTML = parseFloat(
+    prod_array[index].prod_preco * quantValor.innerHTML
+  ).toFixed(2);
+
+  calculaTotal();
+}
+
+// diminuir a quantidade e valor de items no carrinho e o valor total
+function down(index) {
+  let prodAdicionado = document.querySelector(`#prod-add-${index}`);
+  let quantValor = prodAdicionado.querySelector(".quantValor"); // quantidade item no carrinho
+  let precoValor = prodAdicionado.querySelector(".precoValor"); //  valor total item no carrinho
+  let setaBaixo = prodAdicionado.querySelector(".setaBaixo");
+
+  if (quantValor.innerHTML <= 1) {
+    setaBaixo.style.borderTop = "15px solid red";
+
+    setTimeout(() => {
+      setaBaixo.style.borderTop = "15px solid black";
+    }, 200);
+
+    return;
+  }
+
+  quantValor.innerHTML = parseInt(quantValor.innerHTML) - 1;
+  precoValor.innerHTML = parseFloat(
+    quantValor.innerHTML * prod_array[index].prod_preco
+  ).toFixed(2);
+  calculaTotal();
+}
